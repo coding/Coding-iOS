38272c35d9bf148f28d96a4e5e8dccf2bffcb96a
王 原闯 <wangyuanchuang@qq.com>
2014-12-22 15:02:15 +0800

    检查相机和照片的访问权限

--- Coding_iOS/Util/Common/Helper.m
+++ Coding_iOS/Util/Common/Helper.m
@@ -17,7 +17,7 @@
         ALAuthorizationStatus authStatus = [ALAssetsLibrary authorizationStatus];
         if (ALAuthorizationStatusDenied == authStatus ||
             ALAuthorizationStatusRestricted == authStatus) {
-            kTipAlert(@"请在iPhone的“设置->隐私->照片”中打开本应用的访问权限");
+            [self showSettingAlertStr:@"请在iPhone的“设置->隐私->照片”中打开本应用的访问权限"];
             return NO;
         }
     }
@@ -35,12 +35,33 @@
         AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
         if (AVAuthorizationStatusDenied == authStatus ||
             AVAuthorizationStatusRestricted == authStatus) {
-            kTipAlert(@"请在iPhone的“设置->隐私->相机”中打开本应用的访问权限");
+            [self showSettingAlertStr:@"请在iPhone的“设置->隐私->相机”中打开本应用的访问权限"];
             return NO;
         }
     }
     
     return YES;
+}
+
++ (void)showSettingAlertStr:(NSString *)tipStr{
+    //iOS8+系统下可跳转到‘设置’页面，否则只弹出提示窗即可
+    if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_7_1) {
+        UIAlertView *alertView = [UIAlertView bk_alertViewWithTitle:@"提示" message:tipStr];
+        [alertView bk_setCancelButtonWithTitle:@"取消" handler:nil];
+        [alertView bk_addButtonWithTitle:@"设置" handler:nil];
+        [alertView bk_setDidDismissBlock:^(UIAlertView *alert, NSInteger index) {
+            if (index == 1) {
+                UIApplication *app = [UIApplication sharedApplication];
+                NSURL *settingsURL = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+                if ([app canOpenURL:settingsURL]) {
+                    [app openURL:settingsURL];
+                }
+            }
+        }];
+        [alertView show];
+    }else{
+        kTipAlert(@"%@", tipStr);
+    }
 }
 
 @end
