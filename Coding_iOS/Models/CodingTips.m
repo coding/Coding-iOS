e2ab403972fd53f9c2deb8448d84d404ad686f66
王 原闯 <wangyuanchuang@qq.com>
2015-01-23 17:40:33 +0800

    标记已读：看多少标记多少

--- Coding_iOS/Models/CodingTips.m
+++ Coding_iOS/Models/CodingTips.m
@@ -18,7 +18,7 @@
         _canLoadMore = YES;
         _isLoading = _willLoadMore = NO;
         _page = [NSNumber numberWithInteger:1];
-        _pageSize = [NSNumber numberWithInteger:40];
+        _pageSize = [NSNumber numberWithInteger:20];
         _type = 0;
     }
     return self;
@@ -62,18 +62,35 @@
     }
     return params;
 }
+
 - (NSDictionary *)toMarkReadParams{
-    NSDictionary *params;
-    if (_type == 0) {
-        params = @{@"type" : [NSNumber numberWithInteger:0],
-                   @"all" : [NSNumber numberWithInteger:1]};
-    }else if (_type == 1){
-        params = @{@"type" : [NSArray arrayWithObjects:[NSNumber numberWithInteger:1], [NSNumber numberWithInteger:2], nil],
-                   @"all" : [NSNumber numberWithInteger:1]};
-    }else if (_type == 2){
-        params = @{@"type" : [NSNumber numberWithInteger:4],
-                   @"all" : [NSNumber numberWithInteger:1]};
+    if (!self.list || self.list.count <= 0) {
+        return nil;
     }
-    return params;
+    
+    NSMutableArray *unReadArray = [[NSMutableArray alloc] init];
+    for (CodingTip *curTip in self.list) {
+        if (!curTip.status.boolValue) {//未读
+            [unReadArray addObject:curTip.id];
+        }
+    }
+    
+    if (unReadArray.count > 0) {
+        return @{@"id" : unReadArray};
+    }
+    return nil;
+    
+//    NSDictionary *params;
+//    if (_type == 0) {
+//        params = @{@"type" : [NSNumber numberWithInteger:0],
+//                   @"all" : [NSNumber numberWithInteger:1]};
+//    }else if (_type == 1){
+//        params = @{@"type" : [NSArray arrayWithObjects:[NSNumber numberWithInteger:1], [NSNumber numberWithInteger:2], nil],
+//                   @"all" : [NSNumber numberWithInteger:1]};
+//    }else if (_type == 2){
+//        params = @{@"type" : [NSNumber numberWithInteger:4],
+//                   @"all" : [NSNumber numberWithInteger:1]};
+//    }
+//    return params;
 }
 @end
