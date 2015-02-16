3becb563f4bb93ea603eae7e9549b9c65ed1800f
王 原闯 <wangyuanchuang@qq.com>
2015-02-16 12:26:03 +0800

    同上

--- Coding_iOS/AppDelegate.m
+++ Coding_iOS/AppDelegate.m
@@ -138,6 +138,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[UnReadManager shareManager] updateUnRead];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
