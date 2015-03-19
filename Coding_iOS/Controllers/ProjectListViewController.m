26802a8e52f8703458e1120b54961f0f37dc9a3a
王 原闯 <wangyuanchuang@qq.com>
2015-03-19 13:29:31 +0800

    Ta的项目

---
+++ Coding_iOS/Controllers/ProjectListViewController.m
@@ -0,0 +1,13 @@
+//
+//  ProjectListViewController.m
+//  Coding_iOS
+//
+//  Created by Ease on 15/3/19.
+//  Copyright (c) 2015年 Coding. All rights reserved.
+//
+
+#import "ProjectListViewController.h"
+
+@implementation ProjectListViewController
+
+@end
--- Coding_iOS/Controllers/UserInfoDetailViewController.m
+++ Coding_iOS/Controllers/UserInfoDetailViewController.m
@@ -133,9 +133,6 @@
                 }
                 break;
         }
-        
-        
-        
         return cell;
     }
 }
