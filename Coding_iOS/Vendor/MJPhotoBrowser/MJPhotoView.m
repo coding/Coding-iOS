cbeb1b61252649ceab3c8e3f80fc0c560ef89b4e
王 原闯 <wangyuanchuang@qq.com>
2015-04-14 15:02:30 +0800

    MJPhotoView Indicator

--- Coding_iOS/Vendor/MJPhotoBrowser/MJPhotoView.m
+++ Coding_iOS/Vendor/MJPhotoBrowser/MJPhotoView.m
@@ -36,8 +36,8 @@
 		
 		// 属性
 		self.delegate = self;
-		self.showsHorizontalScrollIndicator = NO;
-		self.showsVerticalScrollIndicator = NO;
+//		self.showsHorizontalScrollIndicator = NO;
+//		self.showsVerticalScrollIndicator = NO;
 		self.decelerationRate = UIScrollViewDecelerationRateFast;
 		self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         
