53ebdcc95fb1f935d38973b182aee02e10822259
王 原闯 <wangyuanchuang@qq.com>
2015-05-28 11:56:30 +0800

    错误提示显示不全的bug

--- Coding_iOS/Util/OC_Category/NSObject+Common.m
+++ Coding_iOS/Util/OC_Category/NSObject+Common.m
@@ -55,7 +55,8 @@
     if (tipStr && tipStr.length > 0) {
         MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:kKeyWindow animated:YES];
         hud.mode = MBProgressHUDModeText;
-        hud.labelText = tipStr;
+        hud.detailsLabelFont = [UIFont boldSystemFontOfSize:15.0];
+        hud.detailsLabelText = tipStr;
         hud.margin = 10.f;
         hud.removeFromSuperViewOnHide = YES;
         [hud hide:YES afterDelay:1.0];
