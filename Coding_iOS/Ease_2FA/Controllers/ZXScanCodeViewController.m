eb750e6b25075b59bfde0d7b03069b89861f6b50
王 原闯 <wangyuanchuang@qq.com>
2015-07-06 15:17:57 +0800

    目前仅支持 TOTP 类型的身份验证令牌

--- Coding_iOS/Ease_2FA/Controllers/ZXScanCodeViewController.m
+++ Coding_iOS/Ease_2FA/Controllers/ZXScanCodeViewController.m
@@ -124,15 +124,21 @@
     AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
     
     OTPAuthURL *authURL = [OTPAuthURL authURLWithURL:[NSURL URLWithString:result.text] secret:nil];
-    if (authURL) {
+    if ([authURL isKindOfClass:[TOTPAuthURL class]]) {
         if (self.sucessScanBlock) {
             self.sucessScanBlock(authURL);
         }
         [self.navigationController popViewControllerAnimated:YES];
     }else{
-        NSString *formatString = [self barcodeFormatToString:result.barcodeFormat];
-        NSString *resultString = result.text;
-        NSString *tipStr = [NSString stringWithFormat:@"条码「%@ : %@」不是有效的身份验证令牌条码", formatString, resultString];
+        NSString *tipStr;
+        if (authURL) {
+            tipStr = @"目前仅支持 TOTP 类型的身份验证令牌";
+        }else{
+            NSString *formatString = [self barcodeFormatToString:result.barcodeFormat];
+            NSString *resultString = result.text;
+            tipStr = [NSString stringWithFormat:@"条码「%@ : %@」不是有效的身份验证令牌条码", formatString, resultString];
+        }
+
         UIAlertView *alertV = [UIAlertView bk_alertViewWithTitle:@"无效条码" message:tipStr];
         [alertV bk_addButtonWithTitle:@"重试" handler:^{
             self.scanSucessed = NO;
