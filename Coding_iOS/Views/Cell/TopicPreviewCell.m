b2ab79e290630cc2e174a6582c4dfb6440998ee8
王 原闯 <wangyuanchuang@qq.com>
2015-08-07 19:08:01 +0800

    topic编辑的时候不能点击链接。。cellHeight 的坑

--- Coding_iOS/Views/Cell/TopicPreviewCell.m
+++ Coding_iOS/Views/Cell/TopicPreviewCell.m
@@ -208,7 +208,7 @@
         cellHeight += 8 + [topic.title getHeightWithFont:kTopicContentCell_FontTitle constrainedToSize:CGSizeMake(curWidth, CGFLOAT_MAX)] + 16 + 20;
 
         cellHeight += [ProjectTagsView getHeightForTags:topic.mdLabels];
-        cellHeight += topic.contentHeight + 5;
+        cellHeight += topic.contentHeight + 50;
     }
     return cellHeight;
 }
