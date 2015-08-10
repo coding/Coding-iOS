1c05174e7967417ef1fdcaddc2d3a5d27db6e7eb
王 原闯 <wangyuanchuang@qq.com>
2015-08-10 15:43:43 +0800

    readme.md ~ 文件名小写不能显示的 bug

--- Coding_iOS/Util/Manager/Coding_NetAPIManager.m
+++ Coding_iOS/Util/Manager/Coding_NetAPIManager.m
@@ -1821,8 +1821,11 @@
                 NSString *path = [NSString stringWithFormat:@"api/user/%@/project/%@/git/tree/%@",project.owner_user_name, project.name, defultBranch];
                 [[CodingNetAPIClient sharedJsonClient] requestJsonDataWithPath:path withParams:nil withMethodType:Get andBlock:^(id data, NSError *error) {
                     if (data) {
-                        NSString *readMeHtml = [[[data valueForKey:@"data"] valueForKey:@"readme"] valueForKey:@"preview"];
-                        block(readMeHtml? readMeHtml: @"我们推荐每个项目都新建一个README文件", nil);
+                        id resultData = [[data valueForKey:@"data"] valueForKey:@"readme"];
+                        CodeFile_RealFile *realFile = [NSObject objectOfClass:@"CodeFile_RealFile" fromJSON:resultData];
+                        CodeFile *rCodeFile = [CodeFile codeFileWithRef:defultBranch andPath:realFile.path];
+                        rCodeFile.file = realFile;
+                        block(rCodeFile, nil);
                     }else{
                         block(nil, error);
                     }
