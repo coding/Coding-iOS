639c165d50ab0ee7af058cd7003b4a34d0d96bbf
cjddny <cjddny@qq.com>
2015-11-10 17:02:15 +0800

    数据筛选---数据绑定~

--- Coding_iOS/Views/PopFliterMenu.m
+++ Coding_iOS/Views/PopFliterMenu.m
@@ -12,7 +12,7 @@
 #import "ProjectCount.h"
 
 @interface PopFliterMenu()<UITableViewDataSource,UITableViewDelegate>
-@property (nonatomic, strong, readwrite) NSArray *items;
+@property (nonatomic, strong) NSMutableArray *items;
 @property (nonatomic, strong) XHRealTimeBlur *realTimeBlur;
 @property (nonatomic, strong) UITableView *tableview;
 @property (nonatomic, strong) ProjectCount *pCount;
@@ -24,7 +24,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        self.items = @[@{@"all":@""},@{@"watched":@""},@{@"created":@""},@{@"joined":@""},@{@"stared":@""}];
+        self.items = @[@{@"all":@""},@{@"created":@""},@{@"joined":@""},@{@"watched":@""},@{@"stared":@""}];
         self.pCount=[ProjectCount new];
         [self setup];
     }
@@ -37,7 +37,12 @@
     [[Coding_NetAPIManager sharedManager] request_ProjectsCatergoryAndCounts_WithObj:_pCount andBlock:^(ProjectCount *data, NSError *error) {
         if (data) {
             [weakSelf.pCount configWithProjects:data];
+            [weakSelf updateDateSource:weakSelf.pCount];
             [weakSelf.tableview reloadData];
+        }
+        if(error)
+        {
+            NSLog(@"get count error");
         }
     }];
 }
@@ -63,7 +68,6 @@
     };
     _realTimeBlur.hasTapGestureEnable = YES;
     
-    
     _tableview = ({
         UITableView *tableview=[[UITableView alloc] initWithFrame:self.bounds];
         tableview.backgroundColor=[UIColor clearColor];
@@ -71,15 +75,58 @@
         tableview.dataSource=self;
         [tableview registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
         tableview.tableFooterView=[UIView new];
+        tableview.rowHeight=50;
+        tableview.separatorStyle=UITableViewCellSeparatorStyleNone;
         tableview;
     });
     [self addSubview:_tableview];
+    _tableview.contentOffset=CGPointMake(0, 100);
+
 }
 
+
+#pragma mark -- event & action
 - (void)showMenuAtView:(UIView *)containerView {
     [containerView addSubview:self];
     [_realTimeBlur showBlurViewAtView:self];
 }
+
+- (void)dismissMenu
+{
+    [self removeFromSuperview];
+}
+
+//组装cell标题
+- (NSString*)formatTitleStr:(NSDictionary*)aDic
+{
+    NSString *keyStr=[[aDic allKeys] firstObject];
+    NSMutableString *convertStr=[NSMutableString new];
+    if ([keyStr isEqualToString:@"all"]) {
+        [convertStr appendString:@"全部项目"];
+    }else if ([keyStr isEqualToString:@"created"]) {
+        [convertStr appendString:@"我创建的"];
+    }else if ([keyStr isEqualToString:@"joined"]) {
+        [convertStr appendString:@"我参与的"];
+    }else if ([keyStr isEqualToString:@"watched"]) {
+        [convertStr appendString:@"我关注的"];
+    }else if ([keyStr isEqualToString:@"stared"]) {
+        [convertStr appendString:@"我收藏的"];
+    }else
+    {
+        NSLog(@"-------------error type:%@",keyStr);
+    }
+    if ([[aDic objectForKey:keyStr] length]>0) {
+        [convertStr appendString:[NSString stringWithFormat:@" (%@)",[aDic objectForKey:keyStr]]];
+    }
+    return [convertStr copy];
+}
+
+//更新数据源
+-(void)updateDateSource:(ProjectCount*)pCount
+{
+    _items = @[@{@"all":[pCount.all stringValue]},@{@"created":[pCount.created stringValue]},@{@"joined":[pCount.joined  stringValue]},@{@"watched":[pCount.watched stringValue]},@{@"stared":[pCount.stared stringValue]}];
+}
+
 
 #pragma mark -- uitableviewdelegate & datasource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -88,13 +135,12 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
-    
-    cell.textLabel.text=[[[_items objectAtIndex:indexPath.row] allKeys] firstObject];
+    cell.backgroundColor=[UIColor clearColor];
+    [cell.textLabel setX:20];
+    cell.textLabel.font=[UIFont systemFontOfSize:15];
+    cell.textLabel.textColor=[UIColor colorWithHexString:@"0x222222"];
+    cell.textLabel.text=[self formatTitleStr:[_items objectAtIndex:indexPath.row]];
     return cell;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 100;
 }
 
 @end
