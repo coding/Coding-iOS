2f9f09caf49db34a3d7c58bae800178f6fae7628
cjddny <cjddny@qq.com>
2015-12-08 09:41:36 +0800

    test~

--- Coding_iOS/Views/Cell/ProjectAboutMeListCell.m
+++ Coding_iOS/Views/Cell/ProjectAboutMeListCell.m
@@ -102,6 +102,9 @@
     //Icon
     [_projectIconView sd_setImageWithURL:[_project.icon urlImageWithCodePathResizeToView:_projectIconView] placeholderImage:kPlaceholderCodingSquareWidth(55.0)];
     _privateIconView.hidden =(_project.is_public!=nil)? _project.is_public.boolValue:([_project.fork_count intValue]+[_project.watch_count intValue]==0)?FALSE:TRUE;
+    if (_hidePrivateIcon) {
+        _privateIconView.hidden=TRUE;
+    }
     
     [_privateIconView mas_updateConstraints:^(MASConstraintMaker *make) {
         make.size.mas_equalTo(_privateIconView.hidden?CGSizeZero:CGSizeMake(12, 12));
