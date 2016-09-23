53055221389c84c10c51798717e4c9ee226fac44
王 原闯 <wangyuanchuang@qq.com>
2016-09-23 16:47:26 +0800

    FunctionIntroManager - 改一下，准备上新

--- Coding_iOS/Util/Manager/FunctionIntroManager.m
+++ Coding_iOS/Util/Manager/FunctionIntroManager.m
@@ -7,69 +7,76 @@
 //
 
 #define kIntroPageKey @"intro_page_version"
-#define kIntroPageNum 2
+#define kIntroPageNum 4
 
 #import "FunctionIntroManager.h"
 #import "EAIntroView.h"
 #import "SMPageControl.h"
 #import <NYXImagesKit/NYXImagesKit.h>
 
+@interface FunctionIntroManager ()
+@property (strong, nonatomic) EAIntroView *introView;
+@end
 
 @implementation FunctionIntroManager
 #pragma mark EAIntroPage
-
 + (void)showIntroPage{
-    if (![self needToShowIntro]) {
-        return;
+    if ([self needToShowIntro]) {
+        FunctionIntroManager *manager = [FunctionIntroManager shareManager];
+        [manager.introView showFullscreen];
+        [self markHasBeenShowed];
     }
-    NSMutableArray *pages = [NSMutableArray new];
-    for (int index = 0; index < kIntroPageNum; index ++) {
-        EAIntroPage *page = [self p_pageWithIndex:index];
-        [pages addObject:page];
-    }
-    if (pages.count <= 0) {
-        return;
-    }
-    EAIntroView *introView = [[EAIntroView alloc] initWithFrame:kScreen_Bounds andPages:pages];
-    introView.backgroundColor = [UIColor whiteColor];
-    introView.swipeToExit = YES;
-    introView.scrollView.bounces = YES;
-    
-//    introView.skipButton = [self p_skipButton];
-//    introView.skipButtonY = 20.f + CGRectGetHeight(introView.skipButton.frame);
-//    introView.skipButtonAlignment = EAViewAlignmentCenter;
-    
-    if (pages.count <= 1) {
-        introView.pageControl.hidden = YES;
-    }else{
-        introView.pageControl = [self p_pageControl];
-        introView.pageControlY = 10.f + CGRectGetHeight(introView.pageControl.frame);
-    }
-    [introView showFullscreen];
-    //
-    [self markHasBeenShowed];
 }
 
 + (BOOL)needToShowIntro{
-//    return YES;
-    
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSString *preVersion = [defaults stringForKey:kIntroPageKey];
-    BOOL needToShow = ![preVersion isEqualToString:kVersion_Coding];
-    if (![NSDate isDuringMidAutumn]) {//中秋节期间才显示
-        needToShow = NO;
-    }
+    BOOL needToShow = ![preVersion isEqualToString:kVersionBuild_Coding];
     return needToShow;
 }
 
 + (void)markHasBeenShowed{
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setValue:kVersion_Coding forKey:kIntroPageKey];
+    [defaults setValue:kVersionBuild_Coding forKey:kIntroPageKey];
     [defaults synchronize];
 }
 
 #pragma mark private M
-+ (UIPageControl *)p_pageControl{
+
++ (instancetype)shareManager{
+    static FunctionIntroManager *shared_manager = nil;
+    static dispatch_once_t pred;
+    dispatch_once(&pred, ^{
+        shared_manager = [self new];
+    });
+    return shared_manager;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSMutableArray *pages = [NSMutableArray new];
+        for (int index = 0; index < kIntroPageNum; index ++) {
+            EAIntroPage *page = [self p_pageWithIndex:index];
+            [pages addObject:page];
+        }
+        _introView = [[EAIntroView alloc] initWithFrame:kScreen_Bounds andPages:pages];
+        _introView.backgroundColor = [UIColor whiteColor];
+        _introView.swipeToExit = YES;
+        _introView.scrollView.bounces = YES;
+        _introView.skipButton = nil;
+        if (pages.count <= 1) {
+            _introView.pageControl.hidden = YES;
+        }else{
+            _introView.pageControl = [self p_pageControl];
+            _introView.pageControlY = 10.f + CGRectGetHeight(_introView.pageControl.frame);
+        }
+    }
+    return self;
+}
+
+- (UIPageControl *)p_pageControl{
     UIImage *pageIndicatorImage = [UIImage imageNamed:@"intro_dot_unselected"];
     UIImage *currentPageIndicatorImage = [UIImage imageNamed:@"intro_dot_selected"];
     
@@ -87,36 +94,63 @@
     return (UIPageControl *)pageControl;
 }
 
-+ (UIButton *)p_skipButton{
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, kScreen_Width*0.7, 60)];
-    button.titleLabel.font = [UIFont boldSystemFontOfSize:18];
-    [button setTitle:@"立即体验" forState:UIControlStateNormal];
-    [button setTitleColor:kColorBrandGreen forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor colorWithHexString:@"0x1b9d59"] forState:UIControlStateHighlighted];
+- (UIButton *)p_skipButton{
+    UIButton *button = [UIButton new];
+    [button addTarget:self action:@selector(dismissIntroView) forControlEvents:UIControlEventTouchUpInside];
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:14];
+    button.backgroundColor = kColorNavBG;
+    [button setTitleColor:kColor999 forState:UIControlStateNormal];
+    [button setTitleColor:kColorDDD forState:UIControlStateHighlighted];
+    [button setTitle:@"跳过" forState:UIControlStateNormal];
+    [button doBorderWidth:0 color:nil cornerRadius:15.0];
     return button;
 }
 
-+ (EAIntroPage *)p_pageWithIndex:(NSInteger)index{
+- (UIButton *)p_useImmediatelyButton{
+    UIButton *button = [UIButton new];
+    [button addTarget:self action:@selector(dismissIntroView) forControlEvents:UIControlEventTouchUpInside];
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:20];
+    button.backgroundColor = kColorBrandGreen;
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor lightTextColor] forState:UIControlStateHighlighted];
+    [button setTitle:@"立即体验" forState:UIControlStateNormal];
+    [button doBorderWidth:0 color:nil cornerRadius:4.0];
+    return button;
+}
+
+- (void)dismissIntroView{
+    [self.introView hideWithFadeOutDuration:0.3];
+}
+
+- (EAIntroPage *)p_pageWithIndex:(NSInteger)index{
     NSString *imageName = [NSString stringWithFormat:@"intro_page%ld", (long)index];
-    if (kDevice_Is_iPhone6Plus) {
-        imageName = [imageName stringByAppendingString:@"_ip6+"];
-    }else if (kDevice_Is_iPhone6){
-        imageName = [imageName stringByAppendingString:@"_ip6"];
-    }else if (kDevice_Is_iPhone5){
-        imageName = [imageName stringByAppendingString:@"_ip5"];
-    }else{
-        imageName = [imageName stringByAppendingString:@"_ip4"];
-    }
-    UIImage *image = [UIImage imageNamed:imageName];
-    UIImageView *imageView;
-    if (!image) {
-        imageView = [UIImageView new];
-        imageView.backgroundColor = [UIColor randomColor];
-    }else{
-        imageView = [[UIImageView alloc] initWithImage:image];
-    }
+    imageName = [imageName stringByAppendingString:(kDevice_Is_iPhone6Plus? @"_ip6+":
+                                                    kDevice_Is_iPhone6? @"_ip6":
+                                                    kDevice_Is_iPhone5? @"_ip5":
+                                                    @"_ip4")];
+    UIImageView *imageView = [UIImageView new];
+    imageView.userInteractionEnabled = YES;
     imageView.contentMode = UIViewContentModeScaleAspectFill;
     imageView.clipsToBounds = YES;
+    imageView.image = [UIImage imageNamed:imageName];
+    imageView.backgroundColor = imageView.image? [UIColor clearColor]: [UIColor randomColor];
+    if (index < kIntroPageNum - 1) {
+        UIButton *button = [self p_skipButton];
+        [imageView addSubview:button];
+        [button mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(60, 30));
+            make.top.equalTo(imageView).offset(30);
+            make.right.equalTo(imageView).offset(-20);
+        }];
+    }else{
+        UIButton *button = [self p_useImmediatelyButton];
+        [imageView addSubview:button];
+        [button mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(200, 44));
+            make.centerX.equalTo(imageView);
+            make.bottom.equalTo(imageView).offset(-70);
+        }];
+    }
     EAIntroPage *page = [EAIntroPage pageWithCustomView:imageView];
     return page;
 }
