e050034438606d72e80a54772312052423596c9e
王原闯 <wangyuanchuang@qq.com>
2018-02-06 10:49:22 +0800

    启动页 & 引导页

--- Coding_iOS/Views/EaseStartView.m
+++ Coding_iOS/Views/EaseStartView.m
@@ -10,94 +10,94 @@
 #import <NYXImagesKit/NYXImagesKit.h>
 #import "StartImagesManager.h"
 
+#import "WebViewController.h"
+
 @interface EaseStartView ()
 @property (strong, nonatomic) UIImageView *bgImageView, *logoIconView;
-@property (strong, nonatomic) UILabel *descriptionStrLabel;
+@property (strong, nonatomic) StartImage *st;
 @end
 
 @implementation EaseStartView
 
 + (instancetype)startView{
-    UIImage *logoIcon = [UIImage imageNamed:@"logo_coding_top"];
     StartImage *st = [[StartImagesManager shareManager] randomImage];
-    return [[self alloc] initWithBgImage:st.image logoIcon:logoIcon descriptionStr:st.descriptionStr];
+    return [[self alloc] initWithStartImage:st];
 }
 
-- (instancetype)initWithBgImage:(UIImage *)bgImage logoIcon:(UIImage *)logoIcon descriptionStr:(NSString *)descriptionStr{
+- (instancetype)initWithStartImage:(StartImage *)st{
     self = [super initWithFrame:kScreen_Bounds];
     if (self) {
         //add custom code
-        UIColor *blackColor = [UIColor blackColor];
-        self.backgroundColor = blackColor;
+        UIColor *bgColor = [UIColor whiteColor];
+        self.backgroundColor = bgColor;
         
-        _bgImageView = [[UIImageView alloc] initWithFrame:kScreen_Bounds];
-        _bgImageView.contentMode = UIViewContentModeScaleAspectFill;
+        _bgImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, kScreen_Width, kScreen_Height - 110 - kSafeArea_Bottom)];
+        _bgImageView.clipsToBounds = YES;
         _bgImageView.alpha = 0.0;
+        _bgImageView.contentMode = UIViewContentModeScaleAspectFill;
         [self addSubview:_bgImageView];
-        if (![NSDate isDuringMidAutumn]) {
-            [self addGradientLayerWithColors:@[(id)[blackColor colorWithAlphaComponent:0.4].CGColor, (id)[blackColor colorWithAlphaComponent:0.0].CGColor] locations:nil startPoint:CGPointMake(0.5, 0.0) endPoint:CGPointMake(0.5, 0.4)];
-        }
 
         _logoIconView = [[UIImageView alloc] init];
-        _logoIconView.contentMode = UIViewContentModeScaleAspectFit;
+        _logoIconView.contentMode = UIViewContentModeScaleAspectFill;
+        _logoIconView.image = [UIImage imageNamed:@"logo_coding"];
         [self addSubview:_logoIconView];
-        _descriptionStrLabel = [[UILabel alloc] init];
-        _descriptionStrLabel.font = [UIFont systemFontOfSize:10];
-        _descriptionStrLabel.textColor = [UIColor colorWithWhite:1.0 alpha:0.5];
-        _descriptionStrLabel.textAlignment = NSTextAlignmentCenter;
-        _descriptionStrLabel.alpha = 0.0;
-        [self addSubview:_descriptionStrLabel];
-        
-        [_descriptionStrLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.centerX.equalTo(@[self, _logoIconView]);
-            make.height.mas_equalTo(10);
-            make.bottom.equalTo(self.mas_bottom).offset(-15);
-            make.left.equalTo(self.mas_left).offset(20);
-            make.right.equalTo(self.mas_right).offset(-20);
-        }];
-
         [_logoIconView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.centerX.equalTo(self);
-            make.top.mas_equalTo(kScreen_Height/7);
-            make.width.mas_equalTo(kScreen_Width *2/3);
-            make.height.mas_equalTo(kScreen_Width/4 *2/3);
+            make.centerY.equalTo(self.mas_bottom).offset(-55 - kSafeArea_Bottom);
+            make.size.mas_equalTo(CGSizeMake(65, 65));
         }];
         
-        [self configWithBgImage:bgImage logoIcon:logoIcon descriptionStr:descriptionStr];
+        __weak typeof(self) weakSelf = self;
+        _bgImageView.userInteractionEnabled = YES;
+        [_bgImageView bk_whenTapped:^{
+            [weakSelf bgImageViewTapped];
+        }];
+        
+        self.st = st;
     }
     return self;
 }
 
-- (void)configWithBgImage:(UIImage *)bgImage logoIcon:(UIImage *)logoIcon descriptionStr:(NSString *)descriptionStr{
-    bgImage = [bgImage scaleToSize:[_bgImageView doubleSizeOfFrame] usingMode:NYXResizeModeAspectFill];
+- (void)setSt:(StartImage *)st{
+    _st = st;
+    UIImage *bgImage = [st.image scaleToSize:[_bgImageView doubleSizeOfFrame] usingMode:NYXResizeModeAspectFill];
     self.bgImageView.image = bgImage;
-    self.logoIconView.image = logoIcon;
-    self.descriptionStrLabel.text = descriptionStr;
-    [self updateConstraintsIfNeeded];
+//    [self.bgImageView sd_setImageWithURL:[NSURL URLWithString:self.st.url]];
+}
+
+- (void)bgImageViewTapped{
+    if ([BaseViewController presentingVC].navigationController.viewControllers.count <= 1) {
+        NSString *linkStr = self.st.group.link;
+        if ([linkStr hasPrefix:[NSObject baseURLStr]]) {
+//            [BaseViewController presentLinkStr:linkStr];
+            UIViewController *vc = [BaseViewController analyseVCFromLinkStr:linkStr] ?: [WebViewController webVCWithUrlStr:linkStr];
+            [BaseViewController goToVC:vc];
+        }
+    }
 }
 
 - (void)startAnimationWithCompletionBlock:(void(^)(EaseStartView *easeStartView))completionHandler{
     [kKeyWindow addSubview:self];
     [kKeyWindow bringSubviewToFront:self];
     _bgImageView.alpha = 0.0;
-    _descriptionStrLabel.alpha = 0.0;
 
     @weakify(self);
-    [UIView animateWithDuration:2.0 animations:^{
+    [UIView animateWithDuration:1.0 animations:^{
         @strongify(self);
         self.bgImageView.alpha = 1.0;
-        self.descriptionStrLabel.alpha = 1.0;
     } completion:^(BOOL finished) {
-        [UIView animateWithDuration:0.6 delay:0.3 options:UIViewAnimationOptionCurveEaseIn animations:^{
-            @strongify(self);
-            [self setX:-kScreen_Width];
-        } completion:^(BOOL finished) {
-            @strongify(self);
-            [self removeFromSuperview];
-            if (completionHandler) {
-                completionHandler(self);
-            }
-        }];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [UIView animateWithDuration:0.6 delay:.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+                @strongify(self);
+                [self setX:-kScreen_Width];
+            } completion:^(BOOL finished) {
+                @strongify(self);
+                [self removeFromSuperview];
+                if (completionHandler) {
+                    completionHandler(self);
+                }
+            }];
+        });
     }];
 }
 
