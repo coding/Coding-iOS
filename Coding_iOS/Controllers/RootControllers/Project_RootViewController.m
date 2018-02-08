a3bd0fd7c8003996a8701ea66ba3e8f5111fe936
Easeeeeeeeee <wangyuanchuang@qq.com>
2018-02-08 14:15:35 +0800

    换图片 & 封掉入口 terminal

--- Coding_iOS/Controllers/RootControllers/Project_RootViewController.m
+++ Coding_iOS/Controllers/RootControllers/Project_RootViewController.m
@@ -144,7 +144,7 @@
     [self setupNavBtn];
     self.icarouselScrollEnabled = NO;
     [[StartImagesManager shareManager] handleStartLink];//如果 start_image 有对应的 link 的话，需要进入到相应的 web 页面
-    [self setupTerminalButton];
+//    [self setupTerminalButton];
 }
 
 - (void)setupTerminalButton{
